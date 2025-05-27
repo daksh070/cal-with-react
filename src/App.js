2f8de293ff044b990b5c40b3fe77496e19926d84
daksh070 <dakshpatel0005@gmail.com>
2025-05-27 18:47:55 +0530

    daksh

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import './cal.css';
 
 function App() {
+
+
+  const [input, setInput] = useState('');
+  const handleClick = (value) => {
+    if (value === "=")
+      try {
+        const evalResult = eval(input);
+        setInput(evalResult);
+      } catch (error) {
+        setInput(' Error ');
+      }
+    else if (value === 'Clear') {
+      setInput('');
+    }
+    else {
+      setInput((prev) => prev + value);
+    }
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <h2>React Calculator</h2>
+      <div className='screen'>
+        <input type="text" value={input} placeholder='0' />
+      </div>
+      <div className="button">
+        {['7', '8', '9', '/'].map((item) => (
+          <button key={item} onClick={() => handleClick(item)}>
+            {item}
+          </button>
+        ))}
+        {['4', '5', '6', '*'].map((item) => (
+          <button key={item} onClick={() => handleClick(item)}>
+            {item}
+          </button>
+        ))}
+        {['1', '2', '3', '-'].map((item) => (
+          <button key={item} onClick={() => handleClick(item)}>
+            {item}
+          </button>
+        ))}
+        <button onClick={() => handleClick('0')}>0</button>
+        <button onClick={() => handleClick('+')}>+</button>
+        <button className="equals" onClick={() => handleClick('=')}>=</button>
+        <button className="clear" onClick={() => handleClick('Clear')}>Clear</button>
+      </div>
     </div>
   );
 }
